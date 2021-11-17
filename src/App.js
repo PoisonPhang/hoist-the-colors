58b09de9495cea21f51449b0f5ea34837afc3ae0
PoisonPhang <connormdavis99@ksu.edu>
2021-11-16 18:32:29 -0600

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
-import { Box, Grommet } from 'grommet'
+import { Box, Button, Collapsible, Heading, Grommet, ResponsiveContext } from 'grommet'
+import { Notification } from 'grommet-icons';
+import React, { useState } from 'react';
 
 const AppBar = (props) => (
   <Box
@@ -8,17 +10,43 @@ const AppBar = (props) => (
     align='center'
     justify='between'
     background='brand'
-    pad={{left: 'medium', right: 'small', vertical: 'small'}}
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
     elevation='meduim'
-    style={{zIndex: '1'}}
+    style={{ zIndex: '1' }}
     {...props}
   />
 );
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
-    <Grommet plain>
-      <AppBar>Hello Grommet!</AppBar>
+    <Grommet full themeMode='dark'>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box>
+          <AppBar>
+            <Heading level='3' margin='none'>Hoist The Colors</Heading>
+            <Button icon={<Notification />} onClick={() => setShowSidebar(!showSidebar)} />
+          </AppBar>
+          <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+            <Collapsible direction='horizontal' open={showSidebar}>
+              <Box
+                width='medium'
+                background='light-2'
+                elevation='small'
+                align='center'
+                justify='center'
+              >
+                Sidebar
+              </Box>
+            </Collapsible>
+            <Box flex align='center' justify='center'>
+              Content
+            </Box>
+          </Box>
+        </Box>
+        )}
+      </ResponsiveContext.Consumer>
     </Grommet>
   );
 }
